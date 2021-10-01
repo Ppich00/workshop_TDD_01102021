c026ca3509a678df8f99574846071e1ea5496e7a
Ppich00 <49331622+Ppich00@users.noreply.github.com>
2021-10-01 14:16:23 +0700

    getById

--- src/main/java/com/example/day02/Day02Application.java
+++ src/main/java/com/example/day02/Day02Application.java
@@ -1,13 +1,18 @@
 package com.example.day02;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 public class Day02Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Day02Application.class, args);
-	}
+    public static void main(String[] args) {
+
+        ConfigurableApplicationContext context = SpringApplication.run(Day02Application.class, args);
+        log.info(String.valueOf(context.getBeanDefinitionCount()));
+    }
 
 }
