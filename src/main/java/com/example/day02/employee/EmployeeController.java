a6ad8791eb181819e8ecef68c4201f737d65d85a
Ppich00 <49331622+Ppich00@users.noreply.github.com>
2021-10-01 15:40:23 +0700

    webMvcTest

--- src/main/java/com/example/day02/employee/EmployeeController.java
+++ src/main/java/com/example/day02/employee/EmployeeController.java
@@ -1,14 +1,24 @@
 package com.example.day02.employee;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 
+    @Autowired
+    private EmployeeService employeeService;
+
     @GetMapping("/employee/{id}")
-    public EmployeeResponse getById(@PathVariable String id){
-        return new EmployeeResponse(Integer.parseInt(id),"pichayuth");
+    public EmployeeResponse getById(@PathVariable String id) {
+        // Validate
+        // TODO Next
+        EmployeeResponse result
+                = employeeService.get(Integer.parseInt(id));
+        return result;
     }
+
 }
--- src/main/java/com/example/day02/employee/EmployeeResponse.java
+++ src/main/java/com/example/day02/employee/EmployeeResponse.java
@@ -1,13 +1,32 @@
 package com.example.day02.employee;
 
-import lombok.Data;
+import org.springframework.stereotype.Component;
 
-public @Data class EmployeeResponse{
-	private String name;
+public class EmployeeResponse{
 	private int id;
+	private String name;
+
+	public EmployeeResponse() {
+	}
 
 	public EmployeeResponse(int id, String name) {
 		this.id = id;
 		this.name = name;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public void setId(int id){
+		this.id = id;
+	}
+
+	public int getId(){
+		return id;
 	}
 }
